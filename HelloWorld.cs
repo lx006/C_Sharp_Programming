457cb906e6a5e29a91656cb1820eb0bcd3024439
Laxmi Dawane <76866301+lx006@users.noreply.github.com>
2021-05-26 14:31:22 +0530

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,15 @@
+using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Welcome to c#");
+            Console.Read();
+
+
+        }
+    }
+}
